7fbdd304c8280e9bf65389b2b20d846180266b13
chavezbosquez <chavez.bosquez@gmail.com>
2019-09-19 15:38:23 -0500

    Panel de administración

--- php/actualizar.php
+++ php/actualizar.php
@@ -2,7 +2,6 @@
   /* Actualizar THOR desde GitHub */
   error_reporting(E_ALL); 
   try {
-    echo shell_exec('dir');
     echo shell_exec('C:\Users\operador\Desktop\PortableGit\cmd\git.exe pull origin master');
   } catch (Exception $e) {
     echo $e->getMessage();
