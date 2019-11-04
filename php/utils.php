708913d695329553e209e520335eddeb3c5b92bf
chavezbosquez <chavez.bosquez@gmail.com>
2019-11-03 23:50:20 -0600

    Limpieza de código

--- php/utils.php
+++ php/utils.php
@@ -8,12 +8,14 @@ class Utils {
 		exit('Función init no permitida');
   }
 
+  /* Devuelve la fecha actual en el formato requerido por MySQL */
   public static function getHoy() {
     date_default_timezone_set("America/Mexico_City");
     $hoy = date("Y-m-d H:i:s");//date("y/m/d", time());
     return($hoy);
   }
 
+  /* Elimina espacios en blanco, saltos de línea y tabuladores de las cajas de texto multilínea */
   public static function limpiarTexto($texto) {
     $buffer = str_replace(array("\r", "\n"), ' ', $texto);
     $buffer = trim($buffer);
